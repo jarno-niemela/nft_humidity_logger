1c76fdebadaffdbfbb50c9309d0dbc9774f2409b
jarno-niemela <jargon@iki.fi>
2022-06-12 21:56:26 +0300

    First version of Ruuvi sensor finder

--- find_ruuvi_sensors.py
+++ find_ruuvi_sensors.py
@@ -1,9 +1,9 @@
-    from ruuvitag_sensor.ruuvi import RuuviTagSensor
+from ruuvitag_sensor.ruuvi import RuuviTagSensor
 
-    def handle_data(found_data):
-        print('MAC ' + found_data[0])
-        print(found_data[1])
+def handle_data(found_data):
+    print('MAC ' + found_data[0])
+    print(found_data[1])
 
-    if __name__ == '__main__':
-        print("Scanning devices")
-        RuuviTagSensor.get_datas(handle_data)
+if __name__ == '__main__':
+    print("Scanning devices")
+    RuuviTagSensor.get_datas(handle_data)
