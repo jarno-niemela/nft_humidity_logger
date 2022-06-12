79a510617d7870a17999674c850d42952a15576a
jarno-niemela <jargon@iki.fi>
2022-06-12 22:26:52 +0300

    First version of Ruuvi sensor finder

--- find_ruuvi_sensors.py
+++ find_ruuvi_sensors.py
@@ -8,14 +8,15 @@ run_flag = RunFlag()
 found_sensors=dict()
 
 def handle_data(found_data):
-    print('MAC ' + found_data[0])
-    print(found_data)
     global counter
     counter = counter - 1
     if counter < 0:
         run_flag.running = False
 
-    found_sensors[found_data[0]]=found_data[1]
+    if found_data[0] not in found_sensors:
+        found_sensors[found_data[0]]=found_data[1]
+        print('MAC ' + found_data[0])
+        print(found_data)
 
 # List of macs of sensors which will execute callback function
 RuuviTagSensor.get_datas(handle_data, run_flag=run_flag)
